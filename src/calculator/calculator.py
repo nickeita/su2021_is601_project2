54671f78710ab0fbf371223ac36d2f0ab32f0e31
nickeita <nt89@njit.edu>
2021-07-10 19:07:52 -0400

    Feature: Added methods to the Calculator class
    Task:
    1. Tested methods in the Calculator class
    2. Added screenshots

--- src/calculator/calculator.py
+++ src/calculator/calculator.py
@@ -1,3 +1,40 @@
 class Calculator:
     def __init__(self):
         pass
+
+    # a plus b
+    @staticmethod
+    def add(a, b):
+        return a + b
+
+    # b minus a
+    @staticmethod
+    def sub(a, b):
+        return b - a
+
+    # a multiply by b
+    @staticmethod
+    def mul(a, b):
+        return a * b
+
+    # a divided by b
+    @staticmethod
+    def div(a, b):
+        try:
+            return b / a
+        except ArithmeticError:
+            print('Not Found')
+
+    # a raised to the power of n
+    @staticmethod
+    def npow(a, n):
+        try:
+            return a ** n
+        except ArithmeticError:
+            print('Not Found')
+
+    def sum(self):
+        pass
+
+    def prod(self):
+        pass
--- src/tests/calculatorTest.py
+++ src/tests/calculatorTest.py
@@ -1,4 +1,5 @@
 import unittest
+from math import *
 from calculator import Calculator
 
 
@@ -9,6 +10,27 @@ class CalculatorTest(unittest.TestCase):
 
     def test_object_instance(self):
         self.assertIsInstance(self.calc, Calculator)
+
+    def test_addition_method(self):
+        self.assertEqual(self.calc.add(1, 1), 2)
+
+    def test_subtraction_method(self):
+        self.assertEqual(self.calc.sub(1, 3), 2)
+
+    def test_multiplication_method(self):
+        self.assertEqual(self.calc.mul(1, 2), 2)
+
+    def test_division_method(self):
+        self.assertAlmostEqual(self.calc.div(2, 4), 2.00000000)
+
+    def test_power_method_root(self):
+        self.assertEqual(self.calc.npow(4, 0.5), 2)
+
+    def test_power_method_integer(self):
+        self.assertEqual(self.calc.npow(2, 2), 4)
+
+    def test_power_method_mixed(self):
+        self.assertEqual(self.calc.npow(4, 1.5), 8)
 
 
 if __name__ == '__main__':
